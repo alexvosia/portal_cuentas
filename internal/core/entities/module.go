b834b95e13b7ae5de1be5fc024bdfe48734cc8e8
alexvosia <alexvosia@gmail.com>
2025-05-06 22:42:06 -0600

    temps

--- internal/core/entities/module.go
+++ internal/core/entities/module.go
@@ -11,5 +11,14 @@ type Module struct {
 	FechaModificacion string `json:"fechamodificacion" db:"fechamodificacion"`
 	FechaEliminacion  string `json:"fechaeliminacion" db:"fechaeliminacion"`
 	CoordinadorID     int    `json:"coordinadorid" db:"coordinadorid"`
-	Areas             []Area `json:"areas" db:"areas"`
+	Columna1          string `json:"columna1" db:"columna1"`
+	Columna2          string `json:"columna2" db:"columna2"`
+	Columna3          string `json:"columna3" db:"columna3"`
+	Columna4          string `json:"columna4" db:"columna4"`
+	Columna5          string `json:"columna5" db:"columna5"`
+	Columna6          string `json:"columna6" db:"columna6"`
+	Columna7          string `json:"columna7" db:"columna7"`
+	Columna8          string `json:"columna8" db:"columna8"`
+	Columna9          string `json:"columna9" db:"columna9"`
+	Areas             string `json:"areas" db:"areas"`
 }
--- internal/core/entities/user.go
+++ internal/core/entities/user.go
@@ -3,6 +3,6 @@ package entities
 type User struct {
 	ID       int64  `json:"id"`
 	Username string `json:"username"`
-	Area     Area   `json:"area"`
-	Role     string `json:"role"`
+	Area     int    `json:"area"`
+	Role     int    `json:"role"`
 }
